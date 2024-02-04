65dcb79b299eb3d43043fb1fe247c5a29a730b12
AllanNastin <nastin@tcd.ie>
2024-02-04 16:12:27 +0000

    rollback to transformers code

--- backend/transformers_ner.py
+++ backend/transformers_ner.py
@@ -1,15 +1,16 @@
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 
 # Load pre-trained model and tokenizer
-tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
-model = AutoModelForQuestionAnswering.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
+tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
+model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
 
 # Create a pipeline
-nlp = pipeline("question-answering", model=model, tokenizer=tokenizer)
+nlp = pipeline("ner", model=model, tokenizer=tokenizer)
 
 # Use the model
-context = "A variety of AI systems can generate large quantities of synthetic content..."
-question = "What can AI systems generate?"
+with open("section.txt", "r") as file:
+    example = file.readlines()
+ner_results = [nlp(line) for line in example]
 
-answer = nlp(question=question, context=context)
-print(answer)
+for result in ner_results:
+    print(result)
