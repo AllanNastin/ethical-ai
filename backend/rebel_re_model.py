743b6cc9c41040bf8797fafd745b3458bba02a3a
Dylan Gallagher <gallagdy@tcd.ie>
2024-02-16 18:06:55 +0000

    Fixed bugs introduced in latest refactor

--- backend/rebel_re_model.py
+++ backend/rebel_re_model.py
@@ -1,7 +1,8 @@
-from transformers import pipeline
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 import networkx as nx
 from pyvis.network import Network
 import json
+import re
 
 
 # Function to parse the generated text and extract the triplets (adapted from the REBEL HF model card)
@@ -38,31 +39,58 @@ def extract_triplets(text):
 
 
 if __name__ == "__main__":
-    print("Loading the model...") # Load the model
-    triplet_extractor = pipeline('text2text-generation', model='Babelscape/rebel-large', tokenizer='Babelscape/rebel-large')
+    print("Loading the model...")
+
+    tokenizer = AutoTokenizer.from_pretrained("Babelscape/rebel-large")
+    model = AutoModelForSeq2SeqLM.from_pretrained("Babelscape/rebel-large")
+    gen_kwargs = {
+        "max_length": 256,
+        "length_penalty": 0,
+        "num_beams": 3,
+        "num_return_sequences": 3,
+    }
 
     print("Reading the AI Act file...")
-    # Read the AI Act file
+
     with open("ai_act/ai-act.txt", "r") as file:
         ai_act = file.read()
 
     # Split the AI Act into sentences
-    ai_act = ai_act.replace("\n", " ").split(". ")
-    sentences = [sentence for sentence in ai_act if len(sentence) > 7]
+    ai_act = ai_act.replace("\n", " ")
+    sentences = re.split(r'[.;:]', ai_act)
+    sentences = [s.strip() for s in sentences if len(s.strip()) > 7]
 
     print("Extracting relations...")
     # Extract relations from the sentences
-    relations = []
+    triplets = []
+
     for sentence in sentences:
-        extracted_text = triplet_extractor.tokenizer.batch_decode([triplet_extractor(sentence,  return_tensors=True, return_text=False)[0]["generated_token_ids"]])
-        relations.append(extract_triplets(extracted_text[0]))
+        # Tokenizer text
+        model_inputs = tokenizer(sentence, max_length=256, padding=True, truncation=True, return_tensors='pt')
+
+        # Generate
+        generated_tokens = model.generate(
+            model_inputs["input_ids"].to(model.device),
+            attention_mask=model_inputs["attention_mask"].to(model.device),
+            **gen_kwargs,
+        )
+
+        # Extract text
+        decoded_preds = tokenizer.batch_decode(generated_tokens, skip_special_tokens=False)
+
+        # Extract triplets
+        for idx, sent in enumerate(decoded_preds):
+            triplets.append(extract_triplets(sent))
+
+    print("Filtering the relations...")
+    triplets = [lst for lst in triplets if lst]
 
     print("Creating the knowledge graph...")
     # Create a directed graph
     G = nx.DiGraph()
 
     # Iterate through data to add edges to the graph
-    for item in relations:
+    for item in triplets:
         G.add_edge(item[0]["head"], item[0]["tail"], label=item[0]["type"])
 
     # Initialize PyVis network
@@ -79,7 +107,7 @@ if __name__ == "__main__":
     print("Exporting to JSONL...")
 
     with open('knowledge_graph/relations.jsonl', 'w') as file:
-        for item in relations:
+        for item in triplets:
             json_str = json.dumps(item)
             file.write(json_str + '\n')
 
