21f5759b1905e32790c1513817f6c30ff587c74b
AllanNastin <nastina@tcd.ie>
2024-02-25 21:03:35 +0000

    Remove duplicate import statement in App.test.js

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -3,11 +3,6 @@ jest.mock('react-vis-network-graph', () => ({
 }));
 
 import { render } from '@testing-library/react';
-jest.mock('react-vis-network-graph', () => ({
-  // Your mock implementation here
-}));
-
-import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders without crashing', () => {
