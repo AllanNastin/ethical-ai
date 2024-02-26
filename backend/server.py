218482cfd4202b4e1a6d3edfddd59443daeafcdd
AllanNastin <nastina@tcd.ie>
2024-02-26 21:54:37 +0000

    Add file upload functionality to server.py

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
+from werkzeug.utils import secure_filename
 
 ALLOWED_ORIGINS = {'http://localhost:3000'}
 
@@ -10,16 +12,20 @@ CORS(app, resources={r"/*": {"origins": ALLOWED_ORIGINS}})
 def home():
     return 'Hello, GET Method', 200
 
-@app.route('/process', methods=['POST'])
+
+@app.route('/api/upload', methods=['POST'])
 def process():
-    data = request.json
-    if data and 'message' in data:
-        message = data['message']
-        # Process the message (in this example, just reverse it)
-        processed_message = message[::-1]
-        return jsonify({'processed_message': processed_message}), 200
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part in the request'}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No file part in the request'}), 400
+    if file and file.filename.endswith('.pdf'):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join('/path/to/save', filename))
+        return jsonify({'message': 'File uploaded successfully'}), 200
     else:
-        return jsonify({'error': 'No message received'}), 400
+        return jsonify({'error': 'Invalid file type'}), 400
 
 if __name__ == '__main__':
     print("App is starting")
