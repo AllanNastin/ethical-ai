610c661776d502bb1f501c185d03af35cdc815ab
liz <chowli@tcd.ie>
2024-02-27 19:10:48 +0000

    Revert "Merge branch 'dev' into 41-create-api-endpoints"
    This reverts commit 3dea39b7505a5760e1d4146969fb671aa524ee24, reversing
    changes made to 89247c55f4d2db32f11c2c606341ce10d1eb06c5.

--- app/src/Component/Pages/Home.js
+++ app/src/Component/Pages/Home.js
@@ -2,15 +2,42 @@ import React from 'react';
 import '../../App.css';
 import PDFUpload from '../PDFUpload';
 import { useNavigate } from 'react-router-dom';
-import leftImage from '../Images/home.png'; 
+import leftImage from '../Images/home.png';
 
 function Home({ onUploadSuccess }) {
   const navigate = useNavigate();
 
   const handlePDFUpload = (file) => {
-    onUploadSuccess();
-    navigate("/graph");
+    let formData = new FormData();
+    formData.append('file', file);
+
+    fetch('http://localhost:8000/upload', {
+      method: 'POST',
+      body: formData
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.text(); // Get the response as text
+      })
+      .then(text => {
+        // Split the text by line and parse each line as JSON
+        const jsonlData = text.split('\n').filter(line => line.trim() !== '').map(line => JSON.parse(line));
+        // Store the JSONL data in local storage
+        localStorage.setItem('data', JSON.stringify(jsonlData));
+        onUploadSuccess();
+        navigate("/graph");
+      })
+      .catch(error => console.log('There was a problem with the fetch operation: ' + error.message));
   };
+
+// const handleButtonClick = () => {
+//   // test get
+//   fetch('http://localhost:8000')
+//     .then(response => response.text())
+//     .then(data => console.log(data));
+// }
 
   return (
     <div className="home">
@@ -21,6 +48,8 @@ function Home({ onUploadSuccess }) {
         <div className="home-text">
           <h1>Creating Trustworthy and Ethical AI</h1>
           <p>Test your AI model to see if it is compliant with the EU AI Act</p>
+          {/* sample button to test fetch */}
+          {/* <button onClick={handleButtonClick}>Test Fetch {"(Check Console)"}</button> */}
           <PDFUpload onUpload={handlePDFUpload} />
         </div>
       </div>
