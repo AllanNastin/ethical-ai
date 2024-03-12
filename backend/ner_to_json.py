119da032ee546e4af48b90393e26fcdb8d1504c4
Dylan Gallagher <gallagdy@tcd.ie>
2024-03-12 09:59:17 +0000

    Added dummy predictions to output (matching DocRED)

--- backend/ner_to_json.py
+++ backend/ner_to_json.py
@@ -16,16 +16,28 @@ tokens = [sentences.split() for sentences in examples]
 # Pass data through NER model
 ner_results = ner(examples)
 
+
+def fix_labels(my_str):
+    if my_str == "I-LOC" or my_str == "B-LOC":
+        return "LOC"
+    if my_str == "I-ORG" or my_str == "B-ORG":
+        return "ORG"
+    if my_str == "I-PER" or my_str == "B-PER":
+        return "PER"
+    return "MISC"
+
+
 # Get data in DocRED format
 ner_json_format = [
-    {"title": "Legal text about AI",
-     "sents": [tokens[i]],
+    {"title": "Legal text about AI", 
+     "sents": [tokens[i]], 
      "vertexSet": [[{
-         "name": entity["word"],
-         "sent_id": 0,
-         "pos": [entity["start"], entity["start"] + len(entity["word"].split())],
-         "type": entity["entity"]
-     }] for entity in ner_results[i]]} for i in range(len(examples))]
+         "name": entity["word"], 
+         "sent_id": 0, 
+         "pos": [entity["start"], entity["start"] + len(entity["word"].split())], 
+         "type": fix_labels(entity["entity"])}] for entity in ner_results[i]],
+     "labels": {"head": [], "tail": [], "relation_id": [], "relation_text": [], "evidence": []}}
+    for i in range(len(examples))]
 
 # Export data to JSON
 with open("extracted_entities/dataset/docred/ner_output.json", 'w') as file:
