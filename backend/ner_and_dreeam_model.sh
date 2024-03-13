56ac36afed15509de7bc7d6b00157248c1604a18
Dylan Gallagher <gallagdy@tcd.ie>
2024-03-13 15:52:59 +0000

    Changed script to run new section of code in dreeam model

--- backend/ner_and_dreeam_model.sh
+++ backend/ner_and_dreeam_model.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # Optional: Set up ethicalai environment with Mamba
-# mamba create -n ethicalai
-# mamba install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
-# mamba install transformers
+mamba create -n ethicalai
+mamba activate ethicalai
+mamba install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
+mamba install transformers
 
 python ner_to_json.py
 
-# mamba deactivate
+
 
 # Optional: Set up dreeam environment with Mamba
-# mamba create -n dreeam python=3.8.13
-# mamba activate dreeam
-# mamba install pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit=11.3 -c pytorch
-# mamba install transformers==4.14.1 numpy==1.22.4 opt-einsum==3.3.0 wandb ujson tqdm pandas
+mamba deactivate
+mamba create -n dreeam python=3.8.13
+mamba activate dreeam
+mamba install pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit=11.3 -c pytorch
+mamba install transformers==4.14.1 numpy==1.22.4 opt-einsum==3.3.0 wandb ujson tqdm pandas
 
 
 # Clone DREEAM repo
@@ -33,12 +35,13 @@ python run.py --data_dir dataset/docred \
 --model_name_or_path roberta-large \
 --display_name  testing \
 --load_path dreeam_models \
---eval_mode single \
+--eval_mode infer_only \
 --test_file ner_output.json \
---test_batch_size 8 \
+--test_batch_size 2 \
 --num_labels 4 \
 --evi_thresh 0.2 \
 --num_class 97
+
 
 
 
