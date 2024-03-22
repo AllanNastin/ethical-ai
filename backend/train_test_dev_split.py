2e317243d62d1a708094affefa825db3e3a944cb
Dylan Gallagher <gallagdy@tcd.ie>
2024-03-22 11:02:42 +0000

    Changed train_test_dev_split.py to take command line args

--- backend/train_test_dev_split.py
+++ backend/train_test_dev_split.py
@@ -1,4 +1,6 @@
 import json
+import sys
+import os
 
 
 def train_dev_test_split(path, train_size=0.7, dev_size=0.15):
@@ -20,12 +22,27 @@ def train_dev_test_split(path, train_size=0.7, dev_size=0.15):
     dev_split = data[train_dev_boundary:dev_test_boundary]
     test_split = data[dev_test_boundary:]
 
+    # Make new directory
+    folder_path = "training_data"
+
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+
     # Export the splits to json files
-    with open("train.json", "w") as file:
+    with open(os.path.join(folder_path, "train.json"), "w") as file:
         json.dump(train_split, file, indent=4)
 
-    with open("dev.json", "w") as file:
+    with open(os.path.join(folder_path, "dev.json"), "w") as file:
         json.dump(dev_split, file, indent=4)
 
-    with open("test.json", "w") as file:
+    with open(os.path.join(folder_path, "test.json"), "w") as file:
         json.dump(test_split, file, indent=4)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        train_dev_test_split(sys.argv[1])
+    else:
+        print("Usage: python train_test_dev_split.py path/to/file_you_want_to_split.json")
+
