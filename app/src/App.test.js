42a3a613a4a3da4030a312d782da670d8a50efbd
liz <chowli@tcd.ie>
2024-04-03 14:34:22 +0100

    Revert "fix frontend testing"
    This reverts commit be12636b1bbe3bae52c5aca50dc5d20ba2978f7f.

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,14 +1,5 @@
 jest.mock('react-vis-network-graph', () => ({
-  // Your mock implementation for react-vis-network-graph here
-}));
-
-jest.mock('react-pdftotext', () => ({
-  // Your mock implementation for react-pdftotext here
-  __esModule: true, // this property makes it work
-  default: () => {
-    // Return whatever the module should return
-    return Promise.resolve('mocked text');
-  },
+  // Your mock implementation here
 }));
 
 import { render } from '@testing-library/react';
