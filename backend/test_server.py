fb5d4a29d92f65c054aa203cc45416a6dc59c29e
AllanNastin <nastina@tcd.ie>
2024-04-11 20:40:03 +0100

    remove old server test and wrong python locks

--- backend/test_server.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'http://localhost:8000/upload'
-files = {'file': open('./documentation_input/GoodAI_FactSheet.pdf', 'rb')}
-response = requests.post(url, files=files)
-print(response.status_code)
-print(response.json())
